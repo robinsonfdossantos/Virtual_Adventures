0caba9aa236e05b46dff3f45350464375858d36f
Robinson Dos Santos <robinsonfdossantos@gmail.com>
2023-04-24 22:33:33 +0930

    Adjustments to display maps and images

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,93 @@
-document.getElementById("search-form").onsubmit = (e) => {
-  e.preventDefault();
-  const input = document.getElementById("search-input").value;
-  console.log("searching", input);
-};
 
-const searchAndDisplay = (place) => {
-  // request the api
-  // use js to display
-  const map = document.getElementById("map");
-};
 
-// searchAndDisplay("sydney")
+  document.getElementById("search-form").onsubmit = (e) => {
+    e.preventDefault();
+    const input = document.getElementById("search-input").value;
+    console.log("searching", input);
+  };
+  
+  const searchAndDisplay = (place) => {
+ 
+    const map = document.getElementById("map");
+  };
+  
+  // Ben's Coding section 
+  // Google maps Api key: AIzaSyDbvldWkelK0RqgCBY2Q_onev8mN6ZMSV8     
+  
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+    key: "AIzaSyDbvldWkelK0RqgCBY2Q_onev8mN6ZMSV8",
+    
+  });
+  
+  
+  let map;
+  let service;
+  let infowindow;
+  
+  function initMap() {
+    const sydney = new google.maps.LatLng(-33.867, 151.195);
+  
+    infowindow = new google.maps.InfoWindow();
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: sydney,
+      zoom: 15,
+    });
+  
+    const request = {
+      query: "Museum of Contemporary Art Australia",
+      fields: ["name", "geometry"],
+    };
+  
+    service = new google.maps.places.PlacesService(map);
+    service.findPlaceFromQuery(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+        for (let i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+  
+        map.setCenter(results[0].geometry.location);
+      }
+    });
+  }
+  
+  function createMarker(place) {
+    if (!place.geometry || !place.geometry.location) return;
+  
+    const marker = new google.maps.Marker({
+      map,
+      position: place.geometry.location,
+    });
+  
+    google.maps.event.addListener(marker, "click", () => {
+      infowindow.setContent(place.name || "");
+      infowindow.open(map);
+    });
+  }
+  
+  window.initMap = initMap();
+  
+  //Robinson's Pexels' API key
+  const apiKey = "NyXPP4dWCbpLYnNcxfxBSqiCRCmTlOg0lnpKkCbPn24vwgA0prTsCwJM";
+
+  const form = document.querySelector('search-form');
+  const input = document.querySelector('#search-input');
+  const searchBtn = document.querySelector('#search-btn');
+  const pictures = document.querySelector('#pictures');
+
+  searchBtn.addEventListener('click', () => {
+    const query = input.value;
+    searchPhotos(query);
+  });
+
+  async function searchPhotos(query) {
+    const url = `https://api.pexels.com/v1/search?query=${query}&per_page=12`;
+    const response = await axios.get(url, { headers: { Authorization: apiKey } });
+    const data = response.data;
+
+    pictures.innerHTML = '';
+    data.photos.forEach(photo => {
+      const img = document.createElement('img');
+      img.src = photo.src.medium;
+      pictures.appendChild(img);
+    });
+  }
