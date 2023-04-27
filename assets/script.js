bcab3a36cafe7d8741ffc98ae12ca13213266224
BenC <bcuttance1998@gmail.com>
2023-04-27 18:48:20 +0930

    nil change

--- assets/script.js
+++ assets/script.js
@@ -23,77 +23,89 @@ var searchBar = document.querySelector('#search-input');
   
 });
 
-function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13,
-    mapTypeId: "roadmap",
-  });
-  // Create the search box and link it to the UI element.
-  const input = document.getElementById("search-input");
-  const searchBox = new google.maps.places.SearchBox(input);
 
-  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
+let map;
 
-  let markers = [];
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
 
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    mapTypeId: 'terrain'
   });
 }
 
-window.initAutocomplete = initAutocomplete();
+initMap()
+
+map = new Map(document.getElementById('map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 8
+  
+});
+
+map = new Map(document.getElementById("map"));
+
+// let map;
+// let service;
+// let infowindow;
+
+// function initMap() {
+//   const sydney = new google.maps.LatLng(-33.867, 151.195);
+
+//   infowindow = new google.maps.InfoWindow();
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: sydney,
+//     zoom: 15,
+//   });
+
+//   const request = {
+//     query: "Museum of Contemporary Art Australia",
+//     fields: ["name", "geometry"],
+//   };
+
+//   service = new google.maps.places.PlacesService(map);
+//   service.findPlaceFromQuery(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+//       for (let i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
+// }
+
+// function createMarker(place) {
+//   if (!place.geometry || !place.geometry.location) return;
+
+//   const marker = new google.maps.Marker({
+//     map,
+//     position: place.geometry.location,
+//   });
+
+//   google.maps.event.addListener(marker, "click", () => {
+//     infowindow.setContent(place.name || "");
+//     infowindow.open(map);
+//   });
+// }
+
+// window.initMap = initMap;
+
+/*document.getElementById("search-form").onsubmit = (e) => {
+    e.preventDefault();
+    const input = document.getElementById("search-input").value;
+    console.log("searching", input);
+  };
+  
+  const searchAndDisplay = (place) => {
+    // request the api
+    // use js to display
+    const map = document.getElementById("map");
+  };*/
+  
+  // searchAndDisplay("sydney")
 
 
   const apiKey = "NyXPP4dWCbpLYnNcxfxBSqiCRCmTlOg0lnpKkCbPn24vwgA0prTsCwJM";
