e0fb0b6921b1931bf530dea6d201f26eea216f5d
BenC <bcuttance1998@gmail.com>
2023-05-01 20:16:13 +0930

    added modal on page load with brand logo

--- assets/script.js
+++ assets/script.js
@@ -263,3 +263,28 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function displayModal () {
+modal.style.display = 'block'
+}
+
+displayModal()
